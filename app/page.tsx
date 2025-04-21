9660be677d168f6728eadcc8051f2882f20ecb48
Brijesh <brijeshranu@gmail.com>
2025-04-21 15:01:16 +0530

    Made as discussed

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,28 @@
+import Link from "next/link";
 import MainBanner from "@/src/components/LandingPage/Banner";
-import GetFreeProposal from "@/src/components/LandingPage/getFreeProposal";
-
+import GetFreeProposal from "@/src/components/LandingPage/GetFreeProposal";
+import ResultsDriven from "@/src/components/LandingPage/Resultdriven";
+import DriveRevenue from "@/src/components/LandingPage/DriveRevenue";
+import SeoResults from '@/src/components/LandingPage/SeoResults'
+import WhyGo from '@/src/components/LandingPage/WhyGo'
 
 export default function Home() {
   return (
     <div className="container">
       <MainBanner />
-      {/* <GetFreeProposal/> */}
+      <GetFreeProposal/>
+      <ResultsDriven/>
+      <DriveRevenue/>
+      <div className="whiteBox">
+            <h2>Get ready to grow your Organic Sales</h2>
+            <div className="text-center">
+              <p className="mb-36">You can achieve significant revenue growth and expanded market share through our proven strategies.</p>
+              <Link href="#" className="btn btn-blue">Get Proposal</Link>
+            </div>
+        </div>
+
+        <SeoResults/>
+        <WhyGo/>
     </div>
   );
 }
