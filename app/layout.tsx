f82d6c87301c1befe12d99f2994738db0f73721f
Shani Pandey <shanipandeytech@gmail.com>
2025-04-21 09:17:42 -0700

    remove unused code

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,12 +26,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link
-          rel="stylesheet"
-          href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
-          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-          crossOrigin="anonymous"
-        />
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
